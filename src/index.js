d3936396b2f354f0e20f59b85fbf7748c227dbc9
Yinlong Li <xiaolongbao413@163.com>
2024-03-05 21:39:40 +0800

    Development Environment

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import store from './store'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-import App from './App'
-
-
-const root = createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-)
+  <App />
+);
+
+
