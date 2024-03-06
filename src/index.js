7c7f701146326583a1f4575aab17dfc5fe12130b
Yinlong Li <xiaolongbao413@163.com>
2024-03-06 20:57:38 +0800

    Mock data

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import sum from '@/test'
+// import sum from '@/test'
 
-const total = sum(1, 2)
-console.log(total)
+// const total = sum(1, 2)
+// console.log(total)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
